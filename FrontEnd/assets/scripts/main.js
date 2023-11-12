fb45eac73910529cf98981821c4f4ad486e7b1bc
SteeveMbr1 <steeve.mombrun@outlook.com>
2023-11-13 00:41:39 +0100

    Refactoring: Add SBApp & SBApi classes

--- FrontEnd/assets/scripts/main.js
+++ FrontEnd/assets/scripts/main.js
@@ -1,69 +1,19 @@
-class App {
-
-    static apikey = "";
-    static apiUrl = "http://localhost:5678/api";
-
-    categories = {};
-    works = {};
-
-    async getCategories() {
-        let jsonResponse = null;
-
-        try {
-            const response = await fetch(App.apiUrl + "/categories");
-            jsonResponse = await response.json()
-        } catch (error) {
-            console.log(error)
-        }
-        this.categories = jsonResponse;
-        return jsonResponse;
-    }
-
-    async getWorks() {
-        let jsonResponse = null;
-
-        try {
-            const response = await fetch(App.apiUrl + '/works');
-            jsonResponse = await response.json();
-
-        } catch (error) {
-            console.log(error);
-        }
-        this.works = jsonResponse;
-        return jsonResponse;
-    }
-
-
-    newWorkCard(work) {
-        const Work = document.createElement('figure');
-        const Img = document.createElement('img');
-        const Figcaption = document.createElement('figcaption');
-
-        Img.setAttribute('src', work.imageUrl);
-        Img.setAttribute('data-id', work.id);
-
-        Figcaption.innerHTML = work.title + ' toto';
-
-        Work.appendChild(Img);
-        Work.appendChild(Figcaption);
-        return Work;
-    }
-
-    updateWorksList(worksList) {
-        const Works = document.querySelector('.gallery');
-
-        worksList.forEach(w => {
-            Works.appendChild(
-                this.newWorkCard(w)
-            );
-        });
-    }
-
-}
+import SBApi from './SBApi.js'
+import SBApp from './SBApp.js'
 
 (async () => {
-    const app = new App()
+    const app = new SBApp()
+    const api = new SBApi()
 
-    app.updateWorksList(await app.getWorks());
-    // console.log(await app.getWorks());
+    const Works = await api.getWorks();
+    const Categories = await api.getCategories();
+
+    app.categories.push({ id: 0, name: 'Tous' })
+    app.categories.push(...Categories)
+
+
+    app.updateWorksList(Works);
+    app.loadCategories(app.categories);
+
+
 })()
